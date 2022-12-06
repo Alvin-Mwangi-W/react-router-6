7ae524ee797d3c749b31557b34780a3240fd69c6
Alvin-Mwangi-W <alvin.wacira.m@gmail.com>
2022-12-06 23:50:50 +0300

    final commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Error from './pages/Error'
+import Login from './pages/Login'
+import Products from './pages/Products'
+import SingleProduct from './pages/SingleProduct'
+import SharedLayout from './pages/SharedLayout'
+
 function App() {
+  const [user, setUser] = useState(null);
   return <BrowserRouter>
     <Routes>
-      <Route path="/" element={<h2>Hello Alvin</h2>}></Route>
-      <Route path="testing" element={
-        <div>
-          <h2>Hello</h2>
-          <h3>Testing</h3>
-        </div>
-      }></Route>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />}/>
+        <Route path="about" element={<About />}/>
+        <Route path="products" element={<Products />}/>
+        <Route path="products/:productId" element={<SingleProduct/>}/>
+        <Route path="dashboard" element={<Login user={user}></Login>}/>
+        <Route path="login" element={<Login setUser={setUser}></Login>}/>
+        <Route path="*" element={<Error />}/>
+      </Route>
+      
+      <Route path="singleproduct" element={<SingleProduct />}/>
     </Routes>
   </BrowserRouter>
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 const About = () => {
+  console.log("hello dimwit")
   return (
     <section className='section'>
       <h2>About</h2>
+
+      <Link to='/' className="btn">Back Home</Link>
     </section>
   );
 };
